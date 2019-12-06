6ee7cf224064d3d54801f5b05a6516e724977ec5
Tommy Berman <tomy.berman1@gmail.com>
2019-12-06 23:32:12 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,15 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  data[:gender].reduce({}) { |result, (k,v)|
+    pigeon_names = v
+    
+    pigeon_names.each { |pigeon_name|
+      result[pigeon_name] = {
+        :color => ,
+        :gender => [],
+        :lives =>
+      }
+    }
+    
+    result
+  }
 end
